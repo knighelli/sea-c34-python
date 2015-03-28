692891090ba2144ae5008a75712388d8fb0f6d95
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-28 16:02:01 -0700

    Renamed variable to prompt, edited Except line to take in two exceptions, did not store prompt- just returned it

--- students/KimNighelli/session04/safe_input.py
+++ students/KimNighelli/session04/safe_input.py
@@ -1,5 +1,5 @@
 '''
-This is for Task 10- Improving Raw Input. 
+This is for Task 10- Improving Raw Input 
 
 In this program, I create a function, safe_input() which will 
 modify the potential output of the raw_input() function. The
@@ -10,11 +10,11 @@ will return a none rather than raising those excpetions.
 Kimberlee Nighelli - 28 March 2015
 '''
 
-def safe_input(x):
+def safe_input(prompt):
     try:
-        user_input = raw_input(x)
-    except EOFError, KeyboardInterrupt:
+        return raw_input(prompt)
+    except (EOFError, KeyboardInterrupt):
         return None
 
-    return user_input
 
+safe_input("Hi")
