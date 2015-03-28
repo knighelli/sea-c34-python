f5fd328877ffa5182deb825a03de929ff65f1165
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-28 15:50:42 -0700

    Wrote Except block.

--- students/KimNighelli/session04/safe_input.py
+++ students/KimNighelli/session04/safe_input.py
@@ -4,7 +4,7 @@ This is for Task 10- Improving Raw Input.
 In this program, I create a function, safe_input() which will 
 modify the potential output of the raw_input() function. The
 raw_input() function can generate two exceptions, either an
-EOFError of a KeyboardInturrupt. The safe_input() function 
+EOFError of a KeyboardInterrupt. The safe_input() function 
 will return a none rather than raising those excpetions. 
 
 Kimberlee Nighelli - 28 March 2015
@@ -13,5 +13,8 @@ Kimberlee Nighelli - 28 March 2015
 def safe_input(x):
     try:
         user_input = raw_input(x)
+    except EOFError, KeyboardInterrupt:
+        return None
 
+    return user_input
 
