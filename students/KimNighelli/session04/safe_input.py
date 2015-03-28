de8d2c569db57c3ae71026fa53233e6ae1c6daf6
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-28 15:47:00 -0700

    Wrote docstring, function name, try block.

---
+++ students/KimNighelli/session04/safe_input.py
@@ -0,0 +1,17 @@
+'''
+This is for Task 10- Improving Raw Input. 
+
+In this program, I create a function, safe_input() which will 
+modify the potential output of the raw_input() function. The
+raw_input() function can generate two exceptions, either an
+EOFError of a KeyboardInturrupt. The safe_input() function 
+will return a none rather than raising those excpetions. 
+
+Kimberlee Nighelli - 28 March 2015
+'''
+
+def safe_input(x):
+    try:
+        user_input = raw_input(x)
+
+
