bd912f8b0625f15ac8d17166782e51c903fd6d82
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-29 17:48:28 -0700

    Fixed style errors

--- students/KimNighelli/session04/safe_input.py
+++ students/KimNighelli/session04/safe_input.py
@@ -1,11 +1,11 @@
 '''
-This is for Task 10- Improving Raw Input 
+This is for Task 10- Improving Raw Input
 
-In this program, I create a function, safe_input() which will 
+In this program, I create a function, safe_input() which will
 modify the potential output of the raw_input() function. The
 raw_input() function can generate two exceptions, either an
-EOFError of a KeyboardInterrupt. The safe_input() function 
-will return a none rather than raising those excpetions. 
+EOFError of a KeyboardInterrupt. The safe_input() function
+will return a none rather than raising those excpetions.
 
 Kimberlee Nighelli - 28 March 2015
 '''
