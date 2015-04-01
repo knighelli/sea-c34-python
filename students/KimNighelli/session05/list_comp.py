417975e820c8188455214eb4a7de1fef63ec0162
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-31 18:39:55 -0700

    Wrote initial docstring

---
+++ students/KimNighelli/session05/list_comp.py
@@ -0,0 +1,17 @@
+'''
+This is for Task 13- List Comprehensions
+
+The first part of this assignment was to take an online quiz in Canvas,
+which I have already completed. Please see the quiz "Comprehension
+Comprehension"
+
+The task had the following instruction: Return the number of even ints in
+the given array. Note: the % "mod" operator computes the remainder,
+e.g. 5 % 2 is 1.
+
+Below is the function doing that.
+
+Kimberlee Nighelli - 31 March 2015
+'''
+
+
