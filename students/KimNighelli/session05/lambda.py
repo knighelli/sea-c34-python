42ad38caa9ca4172efbd80976842457a461194ee
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-31 21:18:23 -0700

    Wrote docstring

---
+++ students/KimNighelli/session05/lambda.py
@@ -0,0 +1,12 @@
+'''
+This is for Task 15: Lambda and Keyword Argument Magic
+
+Write a function that returns a list of n functions, such that each one,
+when called, will return the input value, incremented by an increasing number.
+
+Use a for loop, a lambda, and a keyword argument
+
+( Extra credit ): Do it with a list comprehension, instead of a for loop
+
+Kimberlee Nighelli - 31 March 2015
+'''
