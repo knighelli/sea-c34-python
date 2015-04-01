c3ab49337b914cd2bedcf7da940bd78430fa7401
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-31 19:55:56 -0700

    Wrote docstring

---
+++ students/KimNighelli/session05/dict_comp.py
@@ -0,0 +1,9 @@
+'''
+This is for Task 14- Dictionary and Set Comprehensions
+
+This assingment revists some of the tasks we complete in the original
+dict/set lab. In this assignment, we will attempt to complete those tasks
+with list comprehensions
+
+Kimberlee Nighelli - 31 March 2015
+'''
