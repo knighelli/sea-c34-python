a5186a8190603f4efd82b92a7c2dc1fef7034766
Kimberlee Nighelli <knighelli@gmail.com>
2015-04-02 21:18:12 -0700

    Added assertions

--- students/KimNighelli/session05/lambda.py
+++ students/KimNighelli/session05/lambda.py
@@ -14,10 +14,6 @@ Kimberlee Nighelli - 31 March 2015
 # For Loop
 
 
-def func(x, i=0):
-    return x + i
-
-
 def function_builder(n):
     '''
     This function returns a list of n functions such that each one,
@@ -32,10 +28,39 @@ def function_builder(n):
 
 def function_builder_lc(n):
     '''
-    Does the same as above w/ a list comp
+    This function returns a list of n functions such that each one,
+    when called, will return the input value, incremented by an
+    increasing number. This function, unlike the first, completes
+    this task with a list comprehension.
     '''
     a_list = [lambda x, p=i: x+p for i in range(n)]
+    return a_list
+
+'''
+Testing
+'''
 
 if __name__ == "__main__":
+
     a_list = function_builder(10)
-    print a_list[5](3)
+
+    assert a_list[5](3) == 8
+    assert a_list[9](12) == 21
+    assert a_list[0](4) == 4
+
+    a_list = function_builder(3)
+
+    assert a_list[1](2) == 3
+    assert a_list[2](0) == 2
+
+    a_list_lc = function_builder_lc(10)
+
+    assert a_list_lc[5](3) == 8
+    assert a_list_lc[3](2) == 5
+
+    a_list_lc = function_builder_lc(5)
+
+    assert a_list_lc[4](2) == 6
+    assert a_list_lc[1](1) == 2
+
+    print "All tests passed"
