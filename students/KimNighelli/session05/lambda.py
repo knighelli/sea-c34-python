13f0d1c398bad8f4149b0551e36269596bcaef58
Kimberlee Nighelli <knighelli@gmail.com>
2015-04-02 21:20:27 -0700

    Minor style edits- Task complete

--- students/KimNighelli/session05/lambda.py
+++ students/KimNighelli/session05/lambda.py
@@ -6,7 +6,7 @@ when called, will return the input value, incremented by an increasing number.
 
 Use a for loop, a lambda, and a keyword argument
 
-( Extra credit ): Do it with a list comprehension, instead of a for loop
+(Extra credit): Do it with a list comprehension, instead of a for loop
 
 Kimberlee Nighelli - 31 March 2015
 '''
