461e2f753632173a3693d72dd61d31b61cb05825
Kimberlee Nighelli <knighelli@gmail.com>
2015-04-05 22:17:08 -0700

    Wrote questions for classes and oop

---
+++ students/KimNighelli/session05/classes.py
@@ -0,0 +1,48 @@
+'''
+This is for Task 16 - Investigate Session 6 - Classes
+
+In this program, I will pose a variety of question that I had while
+reviewing the session 6 slides on subclasses. Each question will be posed
+in the docstring of the function corresponding to the question.
+
+Kimberlee Nighelli 5 April 2015
+'''
+
+
+def pet_store():
+    '''
+    Can I use a class and methods to illustrate various aspects of
+    the potential pets in a pet store?
+    '''
+
+    class Pet():
+        ''' A class about the pets in a store '''
+
+        def __init__(self, name, species, age, friendly):
+            self.name = name
+            self.species = species
+            self.age = age
+            self.friendly = friendly
+
+        def advertisement(self):
+            ''' Prints out an ad for each animal '''
+            print "{name} is a {age} year old {species} in " \
+                  "need of a home. Are they friendly? {friendly}, "\
+                  "and they are in need of a lot of love.".format(
+                      name=self.name, age=self.age, species=self.species,
+                      friendly=self.friendly)
+
+    penny = Pet("Penny", "Cat", 14, "Yes")
+    gary = Pet("Gary", "Lizard", 3, "No")
+    henry = Pet("Henry", "Cat", 1, "No")
+    jake = Pet("Jake", "Dog", 4, "Yes")
+    charlotte = Pet("Charlotte", "Tarantula", 3, "Maybe")
+    craig = Pet("Craig", "Mouse", 9, "Yes")
+    steven = Pet("Steven", "Dog", 1, "Yes")
+
+    animals = [penny, gary, henry, jake, charlotte, craig, steven]
+    for animal in animals:
+        animal.advertisement()
+
+if __name__ == "__main__":
+    pet_store()
---
+++ students/KimNighelli/session05/oop.py
@@ -0,0 +1,79 @@
+'''
+This file is for Task 16- Exploring Session 6- Object Oriented
+Programming.
+
+In this file I will write two functions that answer some questions about
+Object Oriented Programming that I had while reading through the session 6
+notes. Questions will be posed in each function's docstring.
+
+Kimberlee Nighelli 5 April 2015
+'''
+
+# Question 1
+
+
+def inheritance():
+    '''
+    Question: How does a major aspect of OOP - inheritance - work and what
+    does it look like?
+    This function will show that properties of a class are shared
+    with any class that may be a subclass of it.
+    '''
+
+    class Mammals(object):
+        ''' A class grouping common mammal characteristics. '''
+        legs = 4
+        blood = "warm"
+
+        def __init__(self, demeanor=None):
+            self.demeanor = demeanor
+
+        def about_mammals(self):
+            ''' Returns a descriptor string about a mammal '''
+            print "Mammals have %d legs and are %s "\
+                  "blooded" % (self.legs, self.blood)
+
+    class Dogs(Mammals):
+        ''' A subclass of Mammals describing dogs '''
+        # The Dogs subclass inherits all the attributes of Mammals
+        pass
+
+    dog = Dogs()
+    dog.about_mammals()
+
+
+# Question 2
+
+def person_dict():
+    '''
+    Can I model information about people as an object instead
+    of a dictionary? Instead of having a dictionary within a
+    dictionary (messy), I'll store it as a object.
+    '''
+    d = {}
+
+    class Person(object):
+        ''' A class holding information about people '''
+
+        def __init__(self, name, age, address, email):
+            self.name = name
+            self.age = age
+            self.address = address
+            self.email = email
+
+        def add_to_dict(self):
+            d[self.name] = {}
+            d[self.name]["age"] = self.age
+            d[self.name]["address"] = self.address
+            d[self.name]["email"] = self.email
+
+    joe = Person("Joe Smith", "28", "45 Juniper Ave", "jsmith@netzero.com")
+    mary = Person("Mary Alice", 54, "9 Harvard Rd", "ma@aol.com")
+    joe.add_to_dict()
+    mary.add_to_dict()
+    print d
+
+
+if __name__ == "__main__":
+    inheritance()
+    person_dict()
