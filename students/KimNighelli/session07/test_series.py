26b855493c73bf4940c8a25ac0034ace71b5dcd4
Kimberlee Nighelli <knighelli@gmail.com>
2015-04-09 15:51:13 -0700

    Fixed tons of style issues

--- students/KimNighelli/session07/test_series.py
+++ students/KimNighelli/session07/test_series.py
@@ -1,18 +1,21 @@
 import unittest
 from series import fibonacci, lucas
 
+
 class MyTests(unittest.TestCase):
+    ''' A class to run unittests on series.py '''
 
     def test_fib(self):
-
-        assert fibonacci(-1) == None
+        ''' Test the outputs of the fibonnaci function '''
+        assert fibonacci(-1) is None
         assert fibonacci(0) == 0
         assert fibonacci(1) == 1
         assert fibonacci(5) == 5
         assert fibonacci(10) == 55
 
     def test_lucas(self):
-        assert lucas(-14) == None
+        ''' Test the outputs of the lucas function '''
+        assert lucas(-14) is None
         assert lucas(0) == 2
         assert lucas(1) == 1
         assert lucas(6) == 18
